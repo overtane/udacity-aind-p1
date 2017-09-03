68660e2e45522d5e1a3d155615c1f9d2fb4414fa
Olli Vertanen <olli@vertanen.net>
2017-09-04 02:11:36 +0300

    Initial solution

--- solution.py
+++ solution.py
@@ -7,13 +7,29 @@ def assign_value(values, box, value):
     """
 
     # Don't waste memory appending actions that don't actually change any values
-    if values[box] == value:
-        return values
-
-    values[box] = value
-    if len(value) == 1:
-        assignments.append(values.copy())
+    if values[box] != value:
+        values[box] = value
+        if len(value) == 1:
+            assignments.append(values.copy())
+ 
     return values
+
+rows = 'ABCDEFGHI'
+cols = '123456789'
+
+def cross(A, B):
+    "Cross product of elements in A and elements in B."
+    return [s+t for s in A for t in B]
+
+boxes = cross(rows, cols)
+
+row_units = [cross(r, cols) for r in rows]
+column_units = [cross(rows, c) for c in cols]
+square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
+diag_units = [['A1', 'B2', 'C3', 'D4', 'E5', 'F6', 'G7', 'H8', 'I9'], ['A9', 'B8', 'C7', 'D6', 'E5', 'F4', 'G3', 'H2', 'I1']]
+unitlist = row_units + column_units + square_units + diag_units
+units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
+peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
 
 def naked_twins(values):
     """Eliminate values using the naked twins strategy.
@@ -23,13 +39,28 @@ def naked_twins(values):
     Returns:
         the values dictionary with the naked twins eliminated from peers.
     """
+    
+    twins = []
 
     # Find all instances of naked twins
-    # Eliminate the naked twins as possibilities for their peers
+    for box, digits in values.items():
+        if len(digits) == 2:
+            p = [(box, b) for b in peers[box] if values[b] == digits]   
+            for t in p: 
+                if (t[1],t[0]) not in twins:
+                    twins.append(t)
+    print(twins)        
 
-def cross(A, B):
-    "Cross product of elements in A and elements in B."
-    pass
+    # Eliminate the naked twins as possibilities for their peers
+    for t in twins:
+        for unit in units[t[0]]:
+            if t[1] in unit:
+                to_be_eliminated = [u for u in unit if len(values[u]) > 1 and u != t[0] and u != t[1]]
+                for u in to_be_eliminated:
+                    for digit in values[t[0]]:
+                        values[u] = values[u].replace(digit, '')
+
+    return values
 
 def grid_values(grid):
     """
@@ -38,10 +69,22 @@ def grid_values(grid):
         grid(string) - A grid in string form.
     Returns:
         A grid in dictionary form
-            Keys: The boxes, e.g., 'A1'
-            Values: The value in each box, e.g., '8'. If the box has no value, then the value will be '123456789'.
+        Keys: The boxes, e.g., 'A1'
+        Values: The value in each box, e.g., '8'.
+            If the box has no value, then the value will be '123456789'.
     """
-    pass
+
+    result = []
+
+    assert len(grid) == 81
+
+    for value in grid:
+        if value == '.':
+            result.append('123456789')
+        else:
+            result.append(value)
+                
+    return dict(zip(boxes, result))
 
 def display(values):
     """
@@ -49,19 +92,108 @@ def display(values):
     Args:
         values(dict): The sudoku in dictionary form
     """
-    pass
+    width = 1+max(len(values[s]) for s in boxes)
+    line = '+'.join(['-'*(width*3)]*3)
+    for r in rows:
+        print(''.join(values[r+c].center(width)+('|' if c in '36' else '')
+                      for c in cols))
+        if r in 'CF': print(line)
+    return   
 
 def eliminate(values):
-    pass
+    """Eliminate values from peers of each box with a single value.
+
+    Go through all the boxes, and whenever there is a box with a single value,
+    eliminate this value from the set of values of all its peers.
+
+    Args:
+        values: Sudoku in dictionary form.
+    Returns:
+        Resulting Sudoku in dictionary form after eliminating values.
+    """
+
+    for key, value in values.items():
+        if len(value) == 1:
+            #print(key, value)
+            for p in peers[key]:
+                values[p] = values[p].replace(value, '')
+                #print(value, values[p])
+    return values
 
 def only_choice(values):
-    pass
+    """Finalize all values that are the only choice for a unit.
+
+    Go through all the units, and whenever there is a unit with a value
+    that only fits in one box, assign the value to this box.
+
+    Input: Sudoku in dictionary form.
+    Output: Resulting Sudoku in dictionary form after filling in only choices.
+    """
+
+    digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+    for unit in unitlist:
+        vals = dict((k, []) for k in digits)
+        for box in unit:
+            for digit in values[box]:
+                vals[digit].append(box)
+        for digit, boxes in vals.items():
+            if len(boxes) == 1 and len(values[boxes[0]]) != 1:
+                assign_value(values, boxes[0], digit)
+    return values
 
 def reduce_puzzle(values):
-    pass
+    """
+    Iterate eliminate() and only_choice(). If at some point, there is a box with no available values, return False.
+    If the sudoku is solved, return the sudoku.
+    If after an iteration of both functions, the sudoku remains the same, return the sudoku.
+    Input: A sudoku in dictionary form.
+    Output: The resulting sudoku in dictionary form.
+    """
+    stalled = False
+    while not stalled:
+        # Check how many boxes have a determined value
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+
+        values = eliminate(values)
+        values = only_choice(values)
+        values = naked_twins(values)
+
+        # Check how many boxes have a determined value, to compare
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        # If no new values were added, stop the loop.
+        stalled = solved_values_before == solved_values_after
+        # Sanity check, return False if there is a box with 
+        # zero available values:
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
 
 def search(values):
-    pass
+    """
+    Using depth-first search and propagation, create a search tree 
+    and solve the sudoku.
+    """
+    # First, reduce the puzzle using the previous function
+    values = reduce_puzzle(values)
+    if values is False:
+        return False ## Failed
+    if max(len(values[s]) for s in boxes) == 1: 
+        return values ## Solved!
+        
+    # Choose one of the unfilled squares with the fewest possibilities
+    n, s = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
+    #print(s, values[s])
+    
+    # Now use recursion to solve each one of the resulting sudokus, 
+    # and if one returns a value (not False), return that answer!
+    for value in values[s]:
+        new_sudoku = values.copy()
+        assign_value(new_sudoku, s, value)
+        #new_sudoku[s] = value
+        #print (s, " new attempt ", value)
+        attempt = search(new_sudoku)
+        if attempt:
+            return attempt
 
 def solve(grid):
     """
@@ -72,16 +204,18 @@ def solve(grid):
     Returns:
         The dictionary representation of the final sudoku grid. False if no solution exists.
     """
+    values = grid_values(grid)
+    return search(values);
 
 if __name__ == '__main__':
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
     display(solve(diag_sudoku_grid))
 
-    try:
-        from visualize import visualize_assignments
-        visualize_assignments(assignments)
+    #try:
+    #    from visualize import visualize_assignments
+    #    visualize_assignments(assignments)
 
-    except SystemExit:
-        pass
-    except:
-        print('We could not visualize your board due to a pygame issue. Not a problem! It is not a requirement.')
+    #except SystemExit:
+    #    pass
+    #except:
+    #    print('We could not visualize your board due to a pygame issue. Not a problem! It is not a requirement.')
